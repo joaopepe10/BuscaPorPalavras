db3e3e353cafa329fb81b9b813ce98575b686c46
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-20 18:45:26 -0300

    Fazendo testes com expressoes regulares para fazer uma busca mais precisa

--- src/main/java/org/treino/Main.java
+++ src/main/java/org/treino/Main.java
@@ -8,10 +8,23 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         BuscaDados busca = new BuscaDados();
-        System.out.println(busca.retornaDados());
+        String regex = "jo";
+        String texto = "joao, maria ,joana, marcos, jobovaldo, lucas, vitor";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+
+        System.out.println("TEXTO: " + texto);
+        System.out.println("INDICE: 0123456789");
+        System.out.println("EXPRESSAO: " + matcher.pattern());
+        System.out.println("POSICOES ENCONTRADAS: ");
+        while (matcher.find()){
+            System.out.println(matcher.start() + " ");
+        }
     }
 }
