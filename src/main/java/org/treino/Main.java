b8f965aba28682464b05eb1d054c88b644a8a6cd
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-20 17:34:33 -0300

    Iniciando o projeto para buscar palavras de acordo com oque esta na pagina estabelecida!, instanciando o HttpRequest, pois sera aqui onde irei estabelecer as regas daa minha requisicao, qual verbo utilizar e etc...

---
+++ src/main/java/org/treino/Main.java
@@ -0,0 +1,16 @@
+package org.treino;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("https://pt.wikipedia.org/wiki/Pirâmides_egípcias"))
+                .build();
+    }
+}
