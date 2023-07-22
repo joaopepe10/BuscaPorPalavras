a469bb227412522d5898c3f06d0c701cc0fde559
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-22 14:01:04 -0300

    Descobri uma biblioteca que faz a filtragem das tags, acredito que sera mais viavel com a utilizacao da Jsoup para uma filtragem melhor dos dados, pois necessita menos classes, deixando assim o codigo mais limpo

--- src/main/java/org/treino/config/Teste.java
+++ src/main/java/org/treino/config/Teste.java
@@ -9,15 +9,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Teste {
 
     public static void main(String[] args) throws IOException {
-        /*BuscaDados urlBuscada = new BuscaDados();
+        BuscaDados urlBuscada = new BuscaDados();
         Path path = Path.of("C:\\Users\\João Pedro\\Desktop\\AREA_DE_TRABALHO\\PROGRAMACAO\\PROJETOS\\JAVA\\TreinandoHTTP\\src\\main\\java\\org\\treino\\teste.txt");
         String textoUrl = Files.readString(path);
 
-        List<String> listaDeLinhas = new ArrayList<>();*/
+        List<String> listaDeLinhas = new ArrayList<>();
 
        /* String regex = "([AC]{2}\s)(.*?)([BAC]{3})";
         String textoBuscado = " AC " +
@@ -60,22 +64,25 @@ public class Teste {
         for (String linha:listaDeLinhas){
             System.out.println(linha);
         }*/
-                String texto = " meu nome e joao <a> assfsd title=\"pedro\" </a> meu sobre nome e pires";
+        /*String texto = " meu nome e joao <a> assfsd title=\"pedro\" </a> meu sobre nome e pires";
 
-                // Expressão regular para encontrar o conteúdo entre as tags <a> e o atributo title
-                String regex = "(<a>.*[=]{1})";
+        // Expressão regular para encontrar o conteúdo entre as tags <a> e o atributo title
+        String regex = "(<a>.*[=]{1})";
 
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(texto);
-                String novoTexto = "";
-                while (matcher.find()){
-                    System.out.println(matcher.group(1));
-                    System.out.println("INICIO:" + matcher.start()+" FIM: " + matcher.end());
-                    //COM ESSE METODO EU CONSIGO PASSAR O FILTRO QUE EU DESEJO E QUANDO ENCONTRAR CONSIGO FAZER A SUBSTITUICAO POR ALGUM TEXTO!
-                    novoTexto += "\n" + texto.replaceFirst(regex, "");
-                }
-
-        System.out.println(novoTexto);
-
-            }
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        String novoTexto = "";
+        while (matcher.find()){
+            System.out.println(matcher.group(1));
+            System.out.println("INICIO:" + matcher.start()+" FIM: " + matcher.end());
+            //COM ESSE METODO EU CONSIGO PASSAR O FILTRO QUE EU DESEJO E QUANDO ENCONTRAR CONSIGO FAZER A SUBSTITUICAO POR ALGUM TEXTO!
+            novoTexto += "\n" + texto.replaceFirst(regex, "");
         }
+
+        System.out.println(novoTexto);*/
+
+        Document document = Jsoup.connect("https://pt.wikipedia.org/wiki/Pirâmides_egípcias").get();
+
+        System.out.println(document.getElementsByTag("a"));
+    }
+}
