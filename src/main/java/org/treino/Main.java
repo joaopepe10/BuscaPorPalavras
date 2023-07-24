992248f1349de8962c5937606dfd2fc5013eac59
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-23 22:31:36 -0300

    Criando interface para saber quais metodos implementar

--- src/main/java/org/treino/Main.java
+++ src/main/java/org/treino/Main.java
@@ -1,13 +1,13 @@
 package org.treino;
 
-import org.treino.busca_padrao.RemocaoDeTagsHtml;
+import org.treino.busca_padrao.RemocaoDeTagsHtmlImp;
 
 import java.io.IOException;
 
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        RemocaoDeTagsHtml buscarUrl = new RemocaoDeTagsHtml();
-        System.out.println(buscarUrl.removeTags());
+        RemocaoDeTagsHtmlImp buscarUrl = new RemocaoDeTagsHtmlImp();
+        System.out.println(buscarUrl.removeTags("http://www.sillion.com.br/blog/?p=1760&rdst_srcid=2893878"));
     }
 }
--- src/main/java/org/treino/busca_padrao/RemocaoDeTagsHtml.java
+++ src/main/java/org/treino/busca_padrao/RemocaoDeTagsHtml.java
@@ -1,60 +1,10 @@
 package org.treino.busca_padrao;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
 import java.io.IOException;
 
-public class RemocaoDeTagsHtml {
+public interface RemocaoDeTagsHtml {
 
-    private String url;
+    String removeTags() throws IOException;
 
-    public RemocaoDeTagsHtml() {
-        // URL PADRAO
-        this.url = "https://pt.wikipedia.org/wiki/Pirâmides_egípcias";
-    }
-
-    public RemocaoDeTagsHtml(String url) {
-        this.url = url;
-    }
-
-    public String removeTags() throws IOException{
-        // CONNECT PEGA A URL E A TRANSFORMA EM UM TEXTO INCLUINDO TODAS AS TAGS
-        // URL PADRAO
-        Document urlBuscada = Jsoup.connect("https://pt.wikipedia.org/wiki/Pirâmides_egípcias").get();
-        // PASSADO COMO PARAMETRO A TAG QUE DESEJA OBTER O CONTEUDO
-        String textoUrl = urlBuscada.getElementsByTag("p").toString();
-
-        // COM AUXILIO DO SITE https://regexr.com PARA FAZER AS EXPRESSOES REGULARES E VIZUALIZAR EM TEMPO REAL OQUE A EXPRESSAO SELECIONA
-        // RETIRANDO AS TAGS DO TEXTO PARA UMA LEITURA MELHOR
-        // FAZ A CAPTURA DO CONTEUDO DA TAG A E AGRUPO O TITLE E FAZ A TROCA DESSE CONTEUDO PELO GRUPO 1 QUE ESTA ENTRE PARENTESES
-        textoUrl = textoUrl.replaceAll("<a[^>]*>(.*?)</a>", "$1");
-        textoUrl = textoUrl.replaceAll("<[^>]+>", "");
-
-        return textoUrl;
-    }
-
-    public String removeTags(String url) throws IOException{
-        // CONNECT PEGA A URL E A TRANSFORMA EM UM TEXTO INCLUINDO TODAS AS TAGS
-        Document urlBuscada = Jsoup.connect(url).get();
-        // PASSADO COMO PARAMETRO A TAG QUE DESEJA OBTER O CONTEUDO
-        String textoUrl = urlBuscada.getElementsByTag("p").toString();
-
-        // COM AUXILIO DO SITE https://regexr.com PARA FAZER AS EXPRESSOES REGULARES E VIZUALIZAR EM TEMPO REAL OQUE A EXPRESSAO SELECIONA
-        // RETIRANDO AS TAGS DO TEXTO PARA UMA LEITURA MELHOR
-        // FAZ A CAPTURA DO CONTEUDO DA TAG A E AGRUPO O TITLE E FAZ A TROCA DESSE CONTEUDO PELO GRUPO 1 QUE ESTA ENTRE PARENTESES
-        textoUrl = textoUrl.replaceAll("<a[^>]*>(.*?)</a>", "$1");
-        textoUrl = textoUrl.replaceAll("<[^>]+>", "");
-
-        return textoUrl;
-    }
-
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
+    String removeTags(String url) throws IOException;
 }
