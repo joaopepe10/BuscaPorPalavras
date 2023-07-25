b021c8f9e8afff1b6a2089378260ffc7e415d546
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-25 18:22:04 -0300

    Comecando a fazer alguns testes com jUnit

---
+++ src/test/java/org/treino/busca_padrao/FiltroDePaginaImpTest.java
@@ -0,0 +1,32 @@
+package org.treino.busca_padrao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FiltroDePaginaImpTest {
+
+    @Test
+    void removeTags() {
+    }
+
+    @Test
+    void busca() {
+    }
+
+    @Test
+    void getUrl() {
+    }
+
+    @Test
+    void setUrl() {
+    }
+
+    @Test
+    void getTermo() {
+    }
+
+    @Test
+    void setTermo() {
+    }
+}
