402f7c7e0e8a8eb918081e6bec229608a4998790
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-25 13:10:47 -0300

    Diminuindo o codigo para faciliar a legibilidade

--- src/main/java/org/treino/Main.java
+++ src/main/java/org/treino/Main.java
@@ -14,23 +14,6 @@ public class Main {
         String termo = "bloques de piedra";
         String url = "https://es.wikipedia.org/wiki/Pirámides_de_Egipto";
         FiltroDePaginaImp filtro = new FiltroDePaginaImp(url, termo);
-
-        String x = "bloques de piedra";
-        String[] xy = x.split("\\s");
-        List<String> teste = new ArrayList<>();
-
-
-        /*for (int i = 0; i <= xy.length ; i++){
-            if (i == 0){
-                teste.add(x);
-            }else {
-                teste.add(xy[i-1]);
-            }
-        }
-        for (String i: teste){
-            System.out.println(i);
-        }*/
-
         System.out.println(filtro.buscaDe());
 
     }
