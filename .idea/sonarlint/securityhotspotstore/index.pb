8125ff5691747768686f0d01b84b7422838f94bf
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-26 18:48:27 -0300

    Inserido o nextLine para que limpe o buffer do teclado!

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -10,3 +10,5 @@ p
 :
 
 Dockerfile,6\6\6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- src/main/java/org/treino/RunAplicacao.java
+++ src/main/java/org/treino/RunAplicacao.java
@@ -9,14 +9,18 @@ import java.util.Scanner;
 public class RunAplicacao {
     public static void main(String[] args) throws IOException{
         Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         try{
-            System.out.print("Digite uma url:");
+            System.out.println("Digite uma url:");
             String url = in.next();
-            System.out.print("Digite uma palavra para buscar:");
-            String palavra = in.next();
+
+            in.nextLine();
+
+            System.out.println("Digite uma palavra para buscar:");
+            String palavra = in.nextLine();
+
             FiltroDePaginaImp filtro = new FiltroDePaginaImp(url, palavra);
             System.out.println(filtro.busca());
-            in.close();
         }catch (IOException e){
             System.out.println("Url invalida!");
         }
